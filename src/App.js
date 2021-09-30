14bc45d51f3f5550b5e855e886bafa431e4cdfdd
David Hecker <HECKERDW@GMAIL.COM>
2021-09-29 19:55:07 -0700

    Added edit functionality.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import BestBooks from './BestBooks.js';
-import Button from 'react-bootstrap/Button';
-import BookFormModal from './BookFormModal';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
-import Profile from './Profile.js';
-import Login from './Login.js';
-import EditFormModal from './EditForm';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import BestBooks from "./BestBooks.js";
+import Button from "react-bootstrap/Button";
+import BookFormModal from "./BookFormModal";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import Profile from "./Profile.js";
+import Login from "./Login.js";
+import EditFormModal from "./EditForm";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       user: null,
-      showEditForm: false
+      showEditForm: false,
+      editBookid: null,
     };
   }
 
@@ -49,20 +46,22 @@ class App extends React.Component {
   };
 
   handleDelete = async (please) => {
-    console.log(please);
-
     const deleteURL = "http://localhost:3001/books";
     await axios.delete(deleteURL + "/" + please._id);
     this.getBooks();
   };
 
-  handleEdit = async () => {
+  hideEdit = () => {
     this.setState({
-      showEditForm: true
-    })
-    // const editURL = "http://localhost:3001/books";
-    // await axios.put(editURL + "/" + please._id);
-    // this.getBooks();
+      showEditForm: false,
+    });
+  };
+
+  showEdit = async (book) => {
+    this.setState({
+      showEditForm: true,
+      editBookid: book._id,
+    });
   };
 
   getBooks = async () => {
@@ -71,7 +70,6 @@ class App extends React.Component {
     let booksResponse = await axios.get(booksURL);
     let bookData = booksResponse.data;
     this.setState({ books: bookData });
-    console.log("book data" + booksResponse.data[0].title);
   };
 
   render() {
@@ -81,27 +79,39 @@ class App extends React.Component {
           <Header user={this.state.user} onLogout={this.logoutHandler} />
           <Switch>
             <Route exact path="/">
-              {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
               {this.state.user ? (
                 <>
-                  <BestBooks onEdit={this.handleEdit} onDelete={this.handleDelete} getBooks={this.getBooks} books={this.state.books} />
+                  <BestBooks
+                    onEdit={this.handleEdit}
+                    showEdit={this.showEdit}
+                    onDelete={this.handleDelete}
+                    getBooks={this.getBooks}
+                    books={this.state.books}
+                  />
                   <Button onClick={this.handleAddBookclick}>Add Book</Button>
                   {this.state.showBookForm && (
-                    <BookFormModal hideForm={this.hideForm} getBooks={this.getBooks} />
+                    <BookFormModal
+                      hideForm={this.hideForm}
+                      getBooks={this.getBooks}
+                      onEdit={this.handleEdit}
+                    />
                   )}
-                  {this.state.showEditForm && 
-                    <EditFormModal getBooks={this.getBooks} hideForm={this.handleEdit}/>
-                  }
+                  {this.state.showEditForm && (
+                    <EditFormModal
+                      getBooks={this.getBooks}
+                      hideForm={this.hideEdit}
+                      user={this.state.user}
+                      id={this.state.editBookid}
+                    />
+                  )}
                 </>
               ) : (
-                <Login loginHandler={this.loginHandler}/>
-              )
-              }
+                <Login loginHandler={this.loginHandler} />
+              )}
             </Route>
             <Route path="/profile">
-             <Profile user={this.state.user}/>
+              <Profile user={this.state.user} />
             </Route>
-            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
           </Switch>
           <Footer />
         </Router>
